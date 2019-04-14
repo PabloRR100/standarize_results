c09408df3e933b12eeb01d6cf5e27cd9e0f00f97
PabloRR100 <pabloruizruiz10@gmail.com>
2019-04-14 20:13:25 +0200

    data path

--- data.py
+++ data.py
@@ -21,10 +21,10 @@ def dataloaders(dataset, batch_size):
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
         
-        trainset = torchvision.datasets.CIFAR10(root='../../datasets', train=True, download=True, transform=transform_train)
+        trainset = torchvision.datasets.CIFAR10(root='../datasets', train=True, download=True, transform=transform_train)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
         
-        testset = torchvision.datasets.CIFAR10(root='../../datasets', train=False, download=True, transform=transform_test)
+        testset = torchvision.datasets.CIFAR10(root='../datasets', train=False, download=True, transform=transform_test)
         testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
         
         classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
